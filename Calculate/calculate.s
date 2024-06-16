f7bbc51ca32d80f8756b6cc73def25e2163e832d
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2024-06-16 17:15:43 +0900

    :sparkles: Add sub and mul, udiv

--- Calculate/calculate.s
+++ Calculate/calculate.s
@@ -2,6 +2,7 @@
 .align 2
 
 _start:
+    // ----- ADD -----
     mov X0, #5
     mov X1, #4
     add X2, X0, X1
@@ -28,6 +29,46 @@ _start:
     mov X16, #4
     // Supervisor Call
     svc #0x80
+
+    // ----- SUBTRACT -----
+    mov X0, #2000
+    mov X1, #1995
+    sub X2, X0, X1
+    add X2, X2, #48
+    mov W3, W2
+    strb W3, [sp]
+    mov X0, #1
+    mov X1, sp
+    mov X2, #1
+    mov X16, #4
+    svc #0x80
+
+    // ----- MULTIPLY -----
+    mov X0, #2
+    mov X1, #4
+    mul X2, X0, X1
+    add X2, X2, #48
+    mov W3, W2
+    strb W3, [sp]
+    mov X0, #1
+    mov X1, sp
+    mov X2, #1
+    mov X16, #4
+    svc #0x80
+
+    // ----- DIVIDE -----
+    mov X0, #10
+    mov X1, #2
+    udiv X2, X0, X1
+    add X2, X2, #48
+    mov W3, W2
+    strb W3, [sp]
+    mov X0, #1
+    mov X1, sp
+    mov X2, #1
+    mov X16, #4
+    svc #0x80
+
     // int: rval
     mov X0, #0
     // 1	AUE_EXIT	ALL	{ void exit(int rval); }
