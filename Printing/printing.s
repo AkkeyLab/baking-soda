478bb95acf92f9034b3c351891e7796447efa28d
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2024-10-07 02:33:50 +0900

    :sparkles: Create number print code

---
+++ Printing/printing.s
@@ -0,0 +1,35 @@
+.global _start
+.align 2
+
+_start:
+    // set const
+    mov X0, #2024
+    mov X1, sp
+    mov X2, #10
+
+convert_to_string:
+    udiv X3, X0, X2      // 2024 / 10 = 202
+    msub X4, X3, X2, X0  // 2024 - (202 * 10) = 4 -> 2024 % 10 = 4
+    add X4, X4, #'0'
+
+    // sub X1, X1, #1
+    // strb W4, [X1]
+    strb W4, [X1, #-1]!
+
+    // prepare next loop
+    mov X0, X3
+    // zero check
+    cmp X3, #0
+    b.ne convert_to_string
+
+print_string:
+    mov X0, #1
+    // mov X1, X1
+    mov X2, #4
+    mov X16, #4
+    svc #0x80
+
+exit:
+    mov X0, #0
+    mov X16, #1
+    svc #0x80
